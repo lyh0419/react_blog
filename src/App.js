79231d585f6dbfebdd05eb3e0ee14880574638d8
lyh0419 <lyh960419@icloud.com>
2022-01-13 21:51:07 +0900

    title_request

--- src/App.js
+++ src/App.js
@@ -10,8 +10,13 @@ function App() {
     "영훈이 저녁 추천",
     "태경이 코트 추천",
   ]);
-
+  let [따봉, 따봉변경] = useState(0);
   let posts = "현우 고기 맛집";
+  function 제목바꾸기() {
+    let newArray = [...글제목];
+    newArray[1] = "영훈이 게임 추천";
+    글제목변경(newArray);
+  }
 
   return (
     <div className="App">
@@ -20,13 +25,26 @@ function App() {
       </div>
 
       <div className="list">
-        <h3> {글제목[0]} </h3>
+        <h3>
+          {글제목[0]}{" "}
+          <span
+            onClick={() => {
+              따봉변경(따봉 + 1);
+            }}
+          >
+            따봉👍
+          </span>{" "}
+          {따봉}
+        </h3>
         <p>1월 13일 발행</p>
         <hr />
       </div>
 
       <div className="list">
-        <h3> {글제목[1]} </h3>
+        <h3>
+          {글제목[1]}
+          <button onClick={제목바꾸기}>게임 추천으로 변경</button>
+        </h3>
         <p>1월 14일 발행</p>
         <hr />
       </div>
